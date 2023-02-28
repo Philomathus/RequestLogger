96626d6212e5dbac2515f271ddc39088ead071e3
krzystof <krzystof.wbridges@gmail.com>
2023-02-28 18:37:04 +0800

    Initial Commit

---
+++ src/main/java/com/internal/requestlogger/RequestLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.internal.requestlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestLoggerApplication.class, args);
+	}
+
+}
